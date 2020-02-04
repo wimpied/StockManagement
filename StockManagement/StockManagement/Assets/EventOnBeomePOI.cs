901e2196a27002be3f09c27966f31254c6f80c61
Wimpie De Beer <wdbeer@bscglobal.com>
2020-02-04 13:19:25 +0200

    123

---
+++ StockManagement/StockManagement/Assets/EventOnBeomePOI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventOnBeomePOI : MonoBehaviour
+{
+    public UnityEvent OnBecomePOI;
+
+    private void OnEnable()
+    {
+        GenericCategoryController.BecomePOI += GenericCategoryController_BecomePOI;
+    }
+
+    private void GenericCategoryController_BecomePOI(Transform obj)
+    {
+        OnBecomePOI.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        GenericCategoryController.BecomePOI -= GenericCategoryController_BecomePOI;
+
+    }
+}
